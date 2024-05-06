58a405db5a0653c33355f724482710c0495e19f0
Hariom Chouhan <75517242+hariomchouhan@users.noreply.github.com>
2024-05-06 10:50:29 +0000

    React starter pack with yarn

---
+++ React_starter_pack_with_yarn/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='hariom bold'>Hariom Chouhan</h1>
+      <p className='hariom bold'>GitHub Link: <a href='https://github.com/hariomchouhan' className='hariom'>https://github.com/hariomchouhan</a></p>
+      <p className='hariom bold'>Gmail: <a href='mailto:hariomchouhan430@gmail.com' className='hariom'>hariomchouhan430@gmail.com</a></p>
+    </div>
+  );
+}
+
+export default App;
